478cd2ba8ea6180dc5ecb8ca8984f4df793a2869
Ege Paksoy <egepaskoy@gmail.com>
2023-03-29 17:32:10 +0000

    init commit

--- main.c
+++ main.c
@@ -18,65 +18,57 @@ void part2(void) {
 
   printf("Enter the format of output (S or I): ");
   scanf("%c", &method);
-  if (method == 'I')
-  {
-      printf("Enter the operation(+,-,/,*,%,!,^): ");
-      scanf("%c", &oper);
-  }
-  else if (method == 'S')
-  {
-      printf("Enter m and n values: ");
-      scanf("%d %d", &m,&n);
+  if (method == 'I') {
+    printf("Enter the operation(+,-,/,*,%,!,^): ");
+    scanf("%c", &oper);
+  } else if (method == 'S') {
+    printf("Enter m and n values: ");
+    scanf("%d %d", &m, &n);
   }
   printf("Enter the first operand: ");
   scanf("%d", &n1);
-  if (oper != '!')
-  {
-      printf("Enter the second operand: ");
-      scanf("%d", &n2);
+  if (oper != '!') {
+    printf("Enter the second operand: ");
+    scanf("%d", &n2);
   }
-  switch(oper)
-  {
-      case ('+'):
-        res = n1+n2;
-        break;
-      case ('-'):
-        res = n1-n2;
-        break;
-      case ('*'):
-        res = n1*n2;
-        break;
-      case ('/'):
-        res = n1/n2;
-        break;
-      case ('%'):
-        res = n1%n2;
-        break;
-      case ('^'):
-        res = 1;
-        for (int i = 0; i < n2; i++)
-        {
-            res *= n1;
-        }
-        break;
-      case ('!'):
-        res = 1;
-        for (int i = 1; i <= n1; i++)
-        {
-            res *= i;
-        }
-        break;
+  switch (oper) {
+  case ('+'):
+    res = n1 + n2;
+    break;
+  case ('-'):
+    res = n1 - n2;
+    break;
+  case ('*'):
+    res = n1 * n2;
+    break;
+  case ('/'):
+    res = n1 / n2;
+    break;
+  case ('%'):
+    res = n1 % n2;
+    break;
+  case ('^'):
+    res = 1;
+    for (int i = 0; i < n2; i++) {
+      res *= n1;
+    }
+    break;
+  case ('!'):
+    res = 1;
+    for (int i = 1; i <= n1; i++) {
+      res *= i;
+    }
+    break;
   }
-  if (method == 'I')
-  {
-      if (oper != '!')
-          printf("%d%c%d=%d", n1, oper, n2, res);
-      else
-          printf("%d!=%d", n1, res);
+  if (method == 'I') {
+    if (oper != '!')
+      printf("%d%c%d=%d", n1, oper, n2, res);
+    else
+      printf("%d!=%d", n1, res);
   }
 }
 
 int main(void) {
   part2();
-  return 0;
+#part2nin yarisi tamam return 0;
 }
